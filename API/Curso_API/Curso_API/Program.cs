6d26bbcef163e2d0685a217c50ee6b9cefa67a68
eltonluiz178 <eltonluiz533@gmail.com>
2025-03-11 13:31:27 -0300

    Baixando extensões, feito conexão e tirado arquivos desnecessários

--- API/Curso_API/Curso_API/Program.cs
+++ API/Curso_API/Curso_API/Program.cs
@@ -1,6 +1,18 @@
+using System.Data;
+using Microsoft.Data.SqlClient;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddScoped<IDbConnection>(provider =>
+{
+    SqlConnection connection = new SqlConnection(connectionString);
+    connection.Open();
+    return connection;
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
